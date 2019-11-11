99fb47715c4e085469f98b5ffcdea835df76d615
Julien <julien.seixas@free.fr>
2019-11-11 19:52:33 +0100

    Create UserProfile vue

---
+++ src/views/UserProfile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="UserProfle">
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'UserProfle',
+};
+</script>
