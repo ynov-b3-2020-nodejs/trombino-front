3d74ebf668076c08dacfabd7962de3ad8b7ce5f1
Julien <julien.seixas@free.fr>
2019-11-12 13:47:20 +0100

    Auto stash before merge of "vue/displayUserProfil" and "origin/vue/authForm"

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -4,22 +4,40 @@
       alt="Vue logo"
       src="user.pictureUrl"
     >
+
+    <p>Hi {{ this.$route.params.id }} !</p>
+
+    <div
+      v-for="user in userProfile"
+      :key="user.id.value"
+    >
+      <p>{{ user.name.first }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import GetUsers from '@/components/GetUsers';
 
 export default {
   name: 'UserProfile',
   data() {
     return {
-      user: {
-        firstName: 'Julien',
-        lastName: 'SEIXAS',
-        pictureUrl: 'https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-1/c0.0.160.160a/p160x160/21616554_386031441815958_3852721861291098557_n.jpg?_nc_cat=106&_nc_oc=AQk-iegCxQUerAhm4zOXg3VYTpyH5kVQR-p-1QiR5EYWLt3XrSzw1n75ymHb0s071vsvvMojqYxKmiNlz9Y-PsWW&_nc_ht=scontent-cdt1-1.xx&oh=c80cef3b4f0543abe85925b64f5b4bef&oe=5E44CA8A',
-      },
+      users: [],
     };
+  },
+  component: {
+    GetUsers,
+  },
+  computed: {
+    userProfile() {
+      return this.users.find(userObj => userObj.login.uuid === this.$route.params.id);
+    },
+  },
+  async beforeMount() {
+    const res = await GetUsers.getUsers(20);
+    this.users = res.data.results;
   },
 };
 </script>
