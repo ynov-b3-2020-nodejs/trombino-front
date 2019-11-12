1440fbdd5fe9d4fd48b8672e3be90b60b100da0f
Unknown <geoffrey.parrier@gmail.com>
2019-11-12 11:58:24 +0100

    Merge master on authForm

---
+++ src/components/AuthForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="authform">
+    <el-input
+      v-model="mail"
+      placeholder="Entrez votre mail"
+      clearable
+    />
+    <el-input
+      v-model="pass"
+      placeholder="Entrez votre mot de passe"
+      clearable
+      show-password
+    />
+    <el-button></el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthForm',
+  data() {
+    return {
+      mail: '',
+      pass: '',
+    };
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
