2a7ef4b410ba1f762841318a80ada86b2c2fd150
Julien <julien.seixas@free.fr>
2019-11-17 18:12:07 +0100

    Create editable text components

---
+++ src/components/EditableNumber.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="EditableNumber">
+    <p v-if="!edit">
+      {{ this.value }}
+    </p>
+    <el-button type="primary" icon="el-icon-edit" circle v-on:click="toogleEdit" v-if="!edit"></el-button>
+
+    <el-input-number
+      v-if="edit"
+      v-model="value"
+      @keyup.enter.native="toogleEdit"
+      controls-position="right"
+      @change="handleChange" :min="1" :max="10">
+    </el-input-number>
+    <el-button type="primary" icon="el-icon-check" circle v-on:click="toogleEdit" v-if="edit"></el-button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditableNumber',
+  data () {
+    return {
+      edit: false,
+    };
+  },
+  props: {
+    value: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    toogleEdit () {
+      this.$emit('newValue', this.value);      
+      this.edit ? this.edit = false : this.edit = true;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  #EditableNumber
+  {
+    display: flex;
+    flex-flow: row wrap;
+
+    justify-content: space-between;
+    align-items: center;
+
+    margin: 10px;
+
+    > * {
+      margin: 0 10px;
+    }
+  }
+</style>
---
+++ src/components/EditableText.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="EditableText">
+    <p v-if="!edit">
+      {{ this.value }}
+    </p>
+    <el-button type="primary" icon="el-icon-edit" circle v-on:click="toogleEdit" v-if="!edit"></el-button>
+
+    <el-input
+      v-if="edit"
+      placeholder="Entrez quelque chose"
+      v-model="value"
+      @keyup.enter.native="toogleEdit"
+      clearable>
+    </el-input>
+    <el-button type="primary" icon="el-icon-check" circle v-on:click="toogleEdit" v-if="edit"></el-button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditableText',
+  data () {
+    return {
+      edit: false,
+    };
+  },
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    toogleEdit () {
+      this.$emit('newValue', this.value);      
+      this.edit ? this.edit = false : this.edit = true;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  #EditableText
+  {
+    display: flex;
+    flex-flow: row wrap;
+
+    justify-content: space-between;
+    align-items: center;
+
+    margin: 10px;
+
+    > * {
+      margin: 0 10px;
+    }
+  }
+</style>
